d2623f9360624a9dea5a7507ced358d03d4c4db1
lhrausch <lukerausch@gmail.com>
2022-03-16 14:03:46 -0500

    added editable true

--- re_play_app_api/models.py
+++ re_play_app_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Listing(models.Model):
-    name = models.CharField(max_length=32, default="Blank") 
-    description = models.CharField(max_length=255, default="Blank")
-    price = models.IntegerField(max_length=32, default=0)
-    rarity = models.CharField(max_length=32, default="Blank")
-    condition = models.CharField(max_length=255, default="Blank")
+    name = models.CharField(max_length=32, default="Blank", editable=True) 
+    description = models.CharField(max_length=255, default="Blank", editable=True)
+    price = models.IntegerField(max_length=32, default=0, editable=True)
+    rarity = models.CharField(max_length=32, default="Blank", editable=True)
+    condition = models.CharField(max_length=255, default="Blank", editable=True)
