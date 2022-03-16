f43043e0cc6606e1a2584eacb3b384a3749e882b
lhrausch <lukerausch@gmail.com>
2022-03-16 13:05:31 -0500

    typo fix

--- re_play_app_api/views.py
+++ re_play_app_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ListingSerializer
+from .models import Listing
 
-# Create your views here.
+class ListingList(generics.ListCreateAPIView):
+    queryset = Listing.objects.all().order_by('id')
+    serializer_class = ListingSerializer
+
+class ListingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Listing.objects.all().order_by('id')
+    serializer_class = ListingSerializer
