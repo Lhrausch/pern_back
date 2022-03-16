a8c6743cd425f782849e80fbad5be282f5abaa7d
lhrausch <lukerausch@gmail.com>
2022-03-16 12:53:28 -0500

    typo fix

--- re_play_app_api/urls.py
+++ re_play_app_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('api/DATABASE', views.ListingList.as_view(), name='listing_list'),
-    path('api/DATABASE/<int:pk>', views.ListingDetail.as_view(), name='listing_detail'),
-]
+# urlpatterns = [
+#     path('api/DATABASE', views.ListingList.as_view(), name='listing_list'),
+#     path('api/DATABASE/<int:pk>', views.ListingDetail.as_view(), name='listing_detail'),
+# ]
